5e4ac4a8c831c6aad3351d8478a11f3748b6e173
nikita <ldc.endoscopy@gmail.com>
2024-09-02 18:35:58 +0400

    First commit

---
+++ 001.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset, DataLoader, random_split
+
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
