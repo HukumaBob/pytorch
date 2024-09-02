2a63bc082a510f925cb22ee4bb1d0d734a0ec86e
nikita <ldc.endoscopy@gmail.com>
2024-09-02 21:34:35 +0400

    Add datasets

--- 001.py
+++ 001.py
@@ -1,3 +1,4 @@
+from typing import Any
 import torch
 import torchvision
 from torchvision.datasets import ImageFolder
@@ -9,4 +10,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+class MNISTDataset(Dataset):
+    def __init__(self, path, transform=None) -> None:
+        self.path = path
+        self.transform = transform
 
+        self.len_dataset = 0
+        self.data_list = []
+
+        for path_dir, dir_list, file_list in os.walk(path):
+            if path_dir == path:
+                self.classes = dir_list
+                self.class_to_idx = {
+                    cls_name: i for i, cls_name in enumerate(self.classes)
+                }
+                continue
+            cls = path_dir.split('/')[-1]
+
+            for name_file in file_list:
+                file_path = os.path.join(path_dir, name_file)
+                self.data_list.append((file_path, self.class_to_idx[cls]))
+
+            sel
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index) -> Any:
+        pass
+        
